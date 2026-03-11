03bb8775f966ab5275633329215f608c96426e12
nayanranjanrath <nayanranjanrath5@gmail.com>
2026-03-11 09:25:45 +0530

    foundout why we need hooks

--- firstreactproject/src/App.jsx
+++ firstreactproject/src/App.jsx
@@ -5,10 +5,25 @@
 
 
 function App() {
-  
-
+  let number = 0
+const addvalue=() => {
+  number=number+1
+  console.log("value added ",number)
+}
+const removevalue=()=>{
+  if(number==0){
+   return }
+  else{number=number-1
+    console.log("value decrised",number)
+  }
+}
   return (
-   <h1>hello</h1>
+   <>
+   <h1>hello every one </h1>
+   <h2>counter value {number}</h2>
+   <button onClick={addvalue}>addvalue</button>
+   <button onClick={removevalue}>remove value </button>
+   </>
   )
 }
 
