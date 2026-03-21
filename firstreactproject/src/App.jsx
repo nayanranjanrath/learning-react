141d79c97c21d7ec30d2575d2dccaadaed24b2ae
nayanranjanrath <nayanranjanrath5@gmail.com>
2026-03-21 18:04:56 +0530

    just created some css property

--- firstreactproject/src/App.jsx
+++ firstreactproject/src/App.jsx
@@ -1,5 +1,22 @@
+import React, { useState } from 'react'   // 👈 ADD THIS LIN
+
 function App() {
-  return <h1>Hello World</h1>
+let [colour,setcolour] =useState("red")
+  return (
+<div className='w-full h-screen 'style={{backgroundColor:colour}}>
+<div className='fixed flex flex-wrap justify-center bottom-12 inset-x-0 px-2'>test 
+
+
+</div>
+
+
+
+
+</div>
+
+
+
+  )
 }
 
 export default App
