9afa2cea0530b53f7efc3d43f2bfd212164aab33
nayanranjanrath <nayanranjanrath5@gmail.com>
2026-03-21 17:46:32 +0530

    just fixing

--- firstreactproject/src/App.jsx
+++ firstreactproject/src/App.jsx
@@ -1,32 +1,5 @@
-
-import { useState } from "react"
-
-
-
-
 function App() {
-  let [number,setnumber]=useState(0)
-const addvalue=() => {
-  number=number+1
-  setnumber(number)
-  console.log("value added ",number)
-}
-const removevalue=()=>{
-  if(number==0){
-   return }
-  else{number=number-1
-    setnumber(number)
-    console.log("value decrised",number)
-  }
-}
-  return (
-   <>
-   <h1>hello every one </h1>
-   <h2>counter value {number}</h2>
-   <button onClick={addvalue}>addvalue</button>
-   <button onClick={removevalue}>remove value </button>
-   </>
-  )
+  return <h1>Hello World</h1>
 }
 
 export default App
