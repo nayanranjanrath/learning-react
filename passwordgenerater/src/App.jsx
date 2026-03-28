df2709edfbe70aecdf2067a17676b0003b4bf14a
nayanranjanrath <nayanranjanrath5@gmail.com>
2026-03-28 16:32:43 +0530

    created a function to generate random pass

--- passwordgenerater/src/App.jsx
+++ passwordgenerater/src/App.jsx
@@ -5,11 +5,26 @@ import heroImg from './assets/hero.png'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [length, setlength] = useState(8)
+  const [allownumber, setallownumber] = useState(false)
+  const [allowcharecter, setallowcharacter] = useState(false)
+  const [password, setpassword] = useState(false)
+const passwordgenerator =()=>{
+let pass=" "
+let str ="ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz"
+if(allownumber) str+="1234567890"
+if(allowcharecter) str+="@#$%^&*!"
+for (let i = 1; i < length; i++) {
+  let char =Math.floor(Math.random()*str.length+1)
+  pass =str.charAt(char)
+}
 
+}
   return (
     <>
-      <h1>hello...</h1>
+      <div className=''>
+
+      </div>
     </>
   )
 }
